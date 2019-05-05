0419ac701c29b91eee0bdf953fcce4659a89a4fa
Craig Bomba <talk2cbo@gmail.com>
2019-05-05 11:49:05 -0500

    initially working templates

--- whetherornot_project/settings.py
+++ whetherornot_project/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'home'
