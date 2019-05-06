f774d137a17e7f71047e2f7c212b09a4d5efe4c3
Craig Bomba <talk2cbo@gmail.com>
2019-05-06 14:52:58 -0500

    commit working with weather api and putting json data into pandas dataframe

--- whetherornot/views.py
+++ whetherornot/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponse
+import requests
+import pandas as pd
+from pandas.io.json import json_normalize
+import json
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -9,4 +13,26 @@ class HomePageView(TemplateView):
 
 def hello(request):
     print('------------->>>> hit the hello function again!!')
-    return HttpResponse('<h1>gotcha!!!</h1>')
+    # result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,255657600?units=us&exclude=currently,flags')
+    result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,2017-07-01T15:00:00?units=us&exclude=currently,flags')
+    print(type(result.json()['daily']['data']))
+    print(result.json()['daily']['data'][0]['summary'])
+    # print(type(result.json()))
+    # json_result = json.loads(result.json())
+    # print("json result object: ", type(json_result), json_result)
+    
+    print(result.json())
+    print(type(result.json()))
+    # df = pd.DataFrame(data=(result.json())['daily']['data'])
+    df = pd.read_json(json.dumps(result.json()['daily']['data']), orient='records')
+    print(df.shape)
+    # print(df.columns)
+    # print(df.dtypes)
+    # df = pd.DataFrame(data=json_normalize(result.json()['daily']['data'][0]))
+    print('----------------------dataframe start----------------------------------')
+    print(df)
+    print('----------------------dataframe end------------------------------------')
+    return HttpResponse('\
+        <h1>gotcha!!!</h1>\
+        <p>{}</p>\
+    '.format(df))
