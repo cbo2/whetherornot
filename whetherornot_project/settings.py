3fee164c35ff8e71dd29da562d21d2b7c97f5cf9
Craig Bomba <talk2cbo@gmail.com>
2019-05-16 14:23:20 -0500

    more heroku

--- whetherornot_project/settings.py
+++ whetherornot_project/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'users.CustomUser'
