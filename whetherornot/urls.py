356d7ea796651a2994d5839197f6e6cc1bab42f4
Craig Bomba <talk2cbo@gmail.com>
2019-05-16 13:33:03 -0500

    even more cleanup of old code

--- whetherornot/urls.py
+++ whetherornot/urls.py
@@ -5,10 +5,6 @@ from .views import HomePageView, SearchView
 
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    # path('users/', include('users.urls')),
-    # path('users/', include('django.contrib.auth.urls')),
     path('', HomePageView.as_view(template_name='signup.html'), name='signup'),
     path('search', SearchView.as_view(template_name='search.html'), name='search'),
-    # path('hello', hello, name='hello'),
 ] 
--- whetherornot_project/urls.py
+++ whetherornot_project/urls.py
@@ -22,11 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('users/', include('users.urls')),
-    # path('users/', include('django.contrib.auth.urls')),
-    # path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('', include('whetherornot.urls')),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
-    # path('', HomePageView.as_view(template_name='home.html'), name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
