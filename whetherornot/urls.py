417d10ef7e680be73694896a742c85c5f2eecb9e
Craig Bomba <talk2cbo@gmail.com>
2019-05-16 13:30:57 -0500

    more cleanup of old code

--- whetherornot/urls.py
+++ whetherornot/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path, include
 from django.views.generic.base import TemplateView
-from .views import HomePageView, hello, SearchView
+from .views import HomePageView, SearchView
 
 
 urlpatterns = [
@@ -10,5 +10,5 @@ urlpatterns = [
     # path('users/', include('django.contrib.auth.urls')),
     path('', HomePageView.as_view(template_name='signup.html'), name='signup'),
     path('search', SearchView.as_view(template_name='search.html'), name='search'),
-    path('hello', hello, name='hello'),
+    # path('hello', hello, name='hello'),
 ] 
--- whetherornot/views.py
+++ whetherornot/views.py
@@ -22,34 +22,6 @@ from django import forms
 class LocationForm(forms.Form):
     location = forms.CharField(max_length=100)
 
-# Create your views here.
-# class HomePageView(CreateView):
-#     # print('==========> HomePageView ======================')
-  
-#     def get(self, request, *args, **kwargs):
-#         print('------------------------ LocationForm.get!! ---------------')
-#         context = {'form': LocationForm()}
-#         return render(request, 'home.html', context)
-
-#     def form_valid(self, request, *args, **kwargs):
-#         form = LocationForm(request.POST)
-#         if form.is_valid():
-#             print('------------------------ LocationForm is valid!! ---------------')
-#             # return HttpResponseRedirect(reverse_lazy)
-#         return render(request, 'home.html', {'form': form})
-        
-
-# class HomePageView(FormView):
-#     # print('==========> HomePageView ======================')
-#     template_name = 'home.html'
-#     form_class = LocationForm
-#     success_url = '/hello/'
-
-#     def form_valid(self, form):
-#         context = super(HomePageView.self).get_context_data(*args, **kwargs)
-#         print('==========> HomePageView.form_valid ======================')
-#         return super(HomePageView, self).form_valid(form)
-
 class HomePageView(TemplateView):
     # print('==========> HomePageView ======================')
      template_name = 'search.html'
@@ -134,13 +106,7 @@ class SearchView(FormView):
         print('----------------------dataframe start----------------------------------')
         print(df)
         print('----------------------dataframe end------------------------------------')
-        # ser = df.groupby(['monthday'])['temperatureHigh'].mean().plot(kind='bar')
-        # series = df.groupby(['monthday'])['temperatureHigh']
-        # series = df['temperatureHigh']
-        # ser = series.hist()
-        # ser = series.mean().plot(kine='bar')
-        # print(f'@@@@@@@@@@ ser type is {type(series)} {type(ser)}')
-        # fig = ser.get_figure()
+
         print(f'after dataframe')
 
         # adjust wind speeds by 10 for scale
@@ -193,8 +159,6 @@ class SearchView(FormView):
         plt.title('Cloudiness')
         fig2.savefig(cloudcover_image_file, transparent=True)  # saves the current figure
 
-        # result = hello(context)
-
         # print('sunrise is: ', df.loc(1)['sunriseTime'])
         # sunrise = dt.fromtimestamp(df.loc(1)['sunriseTime']).isoformat()
         # context['sunrise'] = sunrise
@@ -206,13 +170,6 @@ class SearchView(FormView):
         context['precip_image'] = precip_image_filename
         context['cloudcover_image'] = cloudcover_image_filename
         return render(self.request, 'result.html', context)
-
-    # class Meta():
-        # return(super().form_valid(form))
-    # def post(self, request, *args, **kwargs):
-    #     data = request.GET.copy()
-    #     print('******* ', data.get('date'))
-    #     return super(self, request)
 
     def fetch_from_weather_api(self, session, param, latitude, longitude):
         base_url = f'https://api.darksky.net/forecast/{self.dark_sky}/{latitude},{longitude},'
@@ -325,215 +282,3 @@ class SearchView(FormView):
                     except Exception as e:
                         print('caught Exception on response from fetch!')
                         print(e, type(e))
-
-# def hello(request):
-def hello(context):
-    loc, date = context['location'], context['date']
-    print('------------->>>> hit the hello function again!!')
-    print(f'on entry got a location of: {loc} and date of: {date}')
-    # data = request.GET.copy()
-    # print('******* ', data.get('date'))
-    # print('====>>>> date: ', request.POST['date'])
-    # result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,255657600?units=us&exclude=currently,flags')
-    result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,1998-06-30T15:00:00?units=us&exclude=currently,flags')
-    # print(type(result.json()['daily']['data']))
-    # print(result.json()['daily']['data'][0]['summary'])
-    # print(type(result.json()))
-    # json_result = json.loads(result.json())
-    # print("json result object: ", type(json_result), json_result)
-    
-    data_dict = {}
-    print(result.json())
-    print(type(result.json()))
-    df = pd.DataFrame(data=(result.json())['daily']['data'])
-    print('the type is ===============>>>> ', type(result.json()['daily']['data']))
-    print('data is ===============>>>> ', result.json()['daily']['data'])
-    print('the type is ===============>>>> ', type(json.dumps(result.json()['daily']['data'])))
-    print('data is ===============>>>> ', json.dumps(result.json()['daily']['data']))   
-    # data_dict['1998'] = json.dumps(result.json()['daily']['data'])
-    data_dict['1998'] = result.json()['daily']['data'][0]
-    ##df = pd.read_json(json.dumps(result.json()['daily']['data']), orient='records')
-    print(f'shape is: {df.shape}')
-    print(f'description is: \n{df.describe()}')
-    print(f'dataframe: \n{df}')
-
-
-    # get second day worth of data from dark sky weather api
-    result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,1999-07-01T15:00:00?units=us&exclude=currently,flags')
-    # now append to the existing dataframe
-    ## df = df.append(pd.read_json(json.dumps(result.json()['daily']['data']), orient='records'), sort=False)
-    print("**** type is: ", type(result.json()['daily']['data'][0]))
-    data_dict['1999'] = result.json()['daily']['data'][0]
-
-    # get second day worth of data from dark sky weather api
-    result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,2000-07-01T15:00:00?units=us&exclude=currently,flags')
-    # now append to the existing dataframe
-    # df = df.append(pd.read_json(json.dumps(result.json()['daily']['data']), orient='records'), sort=False)
-    data_dict['2000'] = result.json()['daily']['data'][0]
-
-    # get second day worth of data from dark sky weather api
-    result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,2001-07-01T15:00:00?units=us&exclude=currently,flags')
-    # now append to the existing dataframe
-    ##df = df.append(pd.read_json(json.dumps(result.json()['daily']['data']), orient='records'), sort=False)
-    data_dict['2001'] = result.json()['daily']['data'][0]
-    # get second day worth of data from dark sky weather api
-    result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,2002-07-01T15:00:00?units=us&exclude=currently,flags')
-    # now append to the existing dataframe
-    ##df = df.append(pd.read_json(json.dumps(result.json()['daily']['data']), orient='records'), sort=False)
-    data_dict['2002'] = result.json()['daily']['data'][0]
-    # get second day worth of data from dark sky weather api
-    result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,2003-07-01T15:00:00?units=us&exclude=currently,flags')
-    # now append to the existing dataframe
-    ##df = df.append(pd.read_json(json.dumps(result.json()['daily']['data']), orient='records'), sort=False)
-    data_dict['2003'] = result.json()['daily']['data'][0]
-    # get second day worth of data from dark sky weather api
-    result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,2004-07-01T15:00:00?units=us&exclude=currently,flags')
-    # now append to the existing dataframe
-    ##df = df.append(pd.read_json(json.dumps(result.json()['daily']['data']), orient='records'), sort=False)
-    data_dict['2004'] = result.json()['daily']['data'][0]
-    # get second day worth of data from dark sky weather api
-    result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,2005-07-01T15:00:00?units=us&exclude=currently,flags')
-    # now append to the existing dataframe
-    ##df = df.append(pd.read_json(json.dumps(result.json()['daily']['data']), orient='records'), sort=False)
-    data_dict['2005'] = result.json()['daily']['data'][0]
-    # get second day worth of data from dark sky weather api
-    result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,2006-07-01T15:00:00?units=us&exclude=currently,flags')
-    # now append to the existing dataframe
-    ##df = df.append(pd.read_json(json.dumps(result.json()['daily']['data']), orient='records'), sort=False)
-    data_dict['2006'] = result.json()['daily']['data'][0]
-    # get second day worth of data from dark sky weather api
-    result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,2007-07-01T15:00:00?units=us&exclude=currently,flags')
-    # now append to the existing dataframe
-    ##df = df.append(pd.read_json(json.dumps(result.json()['daily']['data']), orient='records'), sort=False)
-    data_dict['2007'] = result.json()['daily']['data'][0]
-    # get second day worth of data from dark sky weather api
-    result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,2008-07-01T15:00:00?units=us&exclude=currently,flags')
-    # now append to the existing dataframe
-    ##df = df.append(pd.read_json(json.dumps(result.json()['daily']['data']), orient='records'), sort=False)
-    data_dict['2008'] = result.json()['daily']['data'][0]
-    # get second day worth of data from dark sky weather api
-    result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,2009-07-01T15:00:00?units=us&exclude=currently,flags')
-    # now append to the existing dataframe
-    ##df = df.append(pd.read_json(json.dumps(result.json()['daily']['data']), orient='records'), sort=False)
-    data_dict['2009'] = result.json()['daily']['data'][0]
-    # get second day worth of data from dark sky weather api
-    result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,2010-07-01T15:00:00?units=us&exclude=currently,flags')
-    # now append to the existing dataframe
-    ##df = df.append(pd.read_json(json.dumps(result.json()['daily']['data']), orient='records'), sort=False)
-    data_dict['2010'] = result.json()['daily']['data'][0]
-    # get second day worth of data from dark sky weather api
-    result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,2011-07-01T15:00:00?units=us&exclude=currently,flags')
-    # now append to the existing dataframe
-    ##df = df.append(pd.read_json(json.dumps(result.json()['daily']['data']), orient='records'), sort=False)
-    data_dict['2011'] = result.json()['daily']['data'][0]
-    # get second day worth of data from dark sky weather api
-    result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,2012-07-01T15:00:00?units=us&exclude=currently,flags')
-    # now append to the existing dataframe
-    ##df = df.append(pd.read_json(json.dumps(result.json()['daily']['data']), orient='records'), sort=False)
-    data_dict['2012'] = result.json()['daily']['data'][0]
-    # get second day worth of data from dark sky weather api
-    result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,2013-07-01T15:00:00?units=us&exclude=currently,flags')
-    # now append to the existing dataframe
-    ##df = df.append(pd.read_json(json.dumps(result.json()['daily']['data']), orient='records'), sort=False)
-    data_dict['2013'] = result.json()['daily']['data'][0]
-    # get second day worth of data from dark sky weather api
-    result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,2014-07-01T15:00:00?units=us&exclude=currently,flags')
-    # now append to the existing dataframe
-    ##df = df.append(pd.read_json(json.dumps(result.json()['daily']['data']), orient='records'), sort=False)
-    data_dict['2014'] = result.json()['daily']['data'][0]
-    # get second day worth of data from dark sky weather api
-    result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,2015-07-01T15:00:00?units=us&exclude=currently,flags')
-    # now append to the existing dataframe
-    ##df = df.append(pd.read_json(json.dumps(result.json()['daily']['data']), orient='records'), sort=False)
-    data_dict['2015'] = result.json()['daily']['data'][0]
-    # get second day worth of data from dark sky weather api
-    result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,2016-07-01T15:00:00?units=us&exclude=currently,flags')
-    # now append to the existing dataframe
-    ##df = df.append(pd.read_json(json.dumps(result.json()['daily']['data']), orient='records'), sort=False)
-    data_dict['2016'] = result.json()['daily']['data'][0]
-    # get second day worth of data from dark sky weather api
-    result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,2017-07-01T15:00:00?units=us&exclude=currently,flags')
-    # now append to the existing dataframe
-    ##df = df.append(pd.read_json(json.dumps(result.json()['daily']['data']), orient='records'), sort=False)
-    data_dict['2017'] = result.json()['daily']['data'][0]
-    # get second day worth of data from dark sky weather api
-    result = requests.get('https://api.darksky.net/forecast/93d657f3bdf48bc91d9977b8e970f9dc/37.4467,25.3289,2018-07-01T15:00:00?units=us&exclude=currently,flags')
-    # now append to the existing dataframe
-    ##df = df.append(pd.read_json(json.dumps(result.json()['daily']['data']), orient='records'), sort=False)
-    data_dict['2018'] = result.json()['daily']['data'][0]
-    print(f'shape is: {df.shape}')
-    print(f'columns are: {df.columns}')
-    print(f'description is: \n{df.describe()}')
-    print(f'info is: \n{df.info()}')
-    # print(f'values: \n{df.values}')
-
-
-    # print(df.columns)
-    # print(df.dtypes)
-    # df = pd.DataFrame(data=json_normalize(result.json()['daily']['data'][0]))
-
-    # let's drop some irrelevant columnss 
-    # df = df.drop(
-    #     ['apparentTemperatureMaxTime', 
-    #     'apparentTemperatureMinTime',
-    #     'dewPoint',
-    #     'icon',
-    #     'moonPhase',
-    #     'pressure',
-    #     'summary',
-    #     'temperatureMaxTime',
-    #     'temperatureMinTime',
-    #     'time',
-    #     'uvIndex',
-    #     'uvIndexTime',
-    #     'windBearing',
-    #     'apparentTemperatureHighTime',
-    #     'apparentTemperatureLowTime',
-    #     'temperatureHighTime',
-    #     'temperatureLowTime',
-    #     'windGustTime',
-    #     'precipIntensity',
-    #     'precipIntensityMax',
-    #     'visibility'
-    #     ], axis=1)
-
-    # return HttpResponse(f'\
-    #     <h1>gotcha!!!</h1>\
-    #     <p>high temp average over 20 years:  {df["temperatureHigh"].mean()}</p>\
-    # ')
- 
-    # return df['temperatureHigh'].mean()
-    df2 = pd.DataFrame.from_dict(data_dict, orient='index')
-    df2 = df2.drop(
-        ['apparentTemperatureMaxTime', 
-        'apparentTemperatureMinTime',
-        'dewPoint',
-        'icon',
-        'moonPhase',
-        'pressure',
-        'summary',
-        'temperatureMaxTime',
-        'temperatureMinTime',
-        'time',
-        'uvIndex',
-        'uvIndexTime',
-        'windBearing',
-        'apparentTemperatureHighTime',
-        'apparentTemperatureLowTime',
-        'temperatureHighTime',
-        'temperatureLowTime',
-        'windGustTime',
-        'precipIntensity',
-        'precipIntensityMax',
-        'visibility'
-        ], axis=1)
-    print(f'shape is: {df2.shape}')
-    print(f'columns (after cleanup) are: {df2.columns}')
-    print(f'description is: \n{df2.describe()}')
-    print(f'the "temperatureHigh" Series: \n{df2["temperatureHigh"]}')
-
-    print('----------------------dataframe2 start----------------------------------')
-    print(df2)
-    print('goofy groupby temphigh = ', df2.groupby('temperatureHigh').mean())
-    print('----------------------dataframe2 end------------------------------------')
-    return df2['temperatureHigh'].mean()
