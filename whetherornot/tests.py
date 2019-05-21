6bd2e6ce0db18a31dcf61ad0bb9c762bc23f0a38
Craig Bomba <talk2cbo@gmail.com>
2019-05-21 13:36:22 -0500

    add initial test cases

--- whetherornot/tests.py
+++ whetherornot/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class WONTests(TestCase):
+    def test_home_page_status(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_search_page_status(self):
+        response = self.client.get('/search')
+        self.assertEqual(response.status_code, 200)
